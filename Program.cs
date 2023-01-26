2e5c4d7091b03d6bf6338f589df2307b092679d9
skye92 <gamecoad92@gmail.com>
2023-01-25 21:07:19 -0600

    Changing int to vartestAdd project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+var input = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("you have chosen " + input);
+
+if (input % 2 == 0)
+{
+    Console.WriteLine("tick");
+}
+
+else
+{
+    Console.WriteLine("tock");
+}
